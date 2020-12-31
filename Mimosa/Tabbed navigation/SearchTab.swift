09416340a977fc5944746faf936ff0d0eef8cb1e
qitianshi <tianshi.qi@icloud.com>
2020-12-31 21:16:10 +0800

    Add tabbed navigation
     - Add tabbed navigation to the main app page.
    
     - Create placeholder tabs for Browse, Library, and Search.

---
+++ Mimosa/Tabbed navigation/SearchTab.swift
@@ -0,0 +1,27 @@
+// SearchTab.swift
+// Mimosa: Working title; non-working project.
+
+// Created on 31/12/20
+// Swift 5.0
+
+// GNU General Public License v3.0
+
+// The Search tab lets users search for formulas by their terms, name, field, and more.
+
+
+import SwiftUI
+
+struct SearchTab: View {
+    var body: some View {
+        
+        // Placeholder content
+        Text("SearchTab.swift")
+        
+    }
+}
+
+struct SearchTab_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTab()
+    }
+}
