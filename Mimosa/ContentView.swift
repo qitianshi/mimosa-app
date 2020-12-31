e028bf7e03ef42ff5deb6f4e5abb49d5583c30ce
qitianshi <tianshi.qi@icloud.com>
2020-12-31 21:01:40 +0800

    Initial Commit

---
+++ Mimosa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mimosa
+//
+//  Created by Tian Shi Qi on 31/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mimosa/MimosaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimosaApp.swift
+//  Mimosa
+//
+//  Created by Tian Shi Qi on 31/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MimosaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
