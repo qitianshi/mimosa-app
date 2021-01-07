9fd170274f0b43b2bd9de25c16417867aa74c36b
qitianshi <tianshi.qi@icloud.com>
2021-01-07 17:22:10 +0800

    Add scrolling content galleries with placeholder data

---
+++ Mimosa/Navigation tabs/Browse tab/BT_ListRow.swift
@@ -0,0 +1,66 @@
+// BT_ListRow.swift
+// Mimosa: Working title; non-working project.
+
+// Created on 7/1/21
+// Swift 5.0
+
+// GNU General Public License v3.0
+
+// A single row in the NavigationView List in BrowseTab.
+
+
+import SwiftUI
+
+/// Model for information that describes each list row.
+struct BT_ListItem: Identifiable {
+    var id = UUID()
+    var heading: String
+    var previewType: PreviewType
+}
+
+struct BT_ListRow: View {
+    
+    let heading: String
+    let previewType: PreviewType
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text(self.heading)
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                
+                LazyHStack {
+                    ForEach((0...3), id: \.self) { i in
+                        
+                        // TODO: Placeholder content
+                        GV_ContentPreview(heading: "Preview \(i + 1)", type: self.previewType)
+                            .frame(width: 200, height: 120)
+                        
+                    }
+                }
+                .padding(.horizontal)       // Adds some space at either end of the horizontal scroll.
+                
+            }
+            
+        }
+        .padding(.vertical)
+        
+    }
+    
+}
+
+struct BT_ListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        // Placeholder values for heading and previewType.
+        BT_ListRow(heading: "Formulas", previewType: .formula)
+            .frame(height: 200)
+            .previewLayout(.sizeThatFits)
+        
+    }
+}
---
+++ Mimosa/Navigation tabs/Browse tab/BrowseTab.swift
@@ -0,0 +1,46 @@
+// BrowseTab.swift
+// Mimosa: Working title; non-working project.
+
+// Created on 31/12/20
+// Swift 5.0
+
+// GNU General Public License v3.0
+
+// The Browse tab showcases formulas and recommends them based on user activity.
+
+
+import SwiftUI
+
+struct BrowseTab: View {
+    
+    // Content of each list row is stored in an array so they can be generated programmatically.
+    private static let listHeadings = [BT_ListItem(heading: "Formulas",
+                                                   previewType: .formula),
+                                       BT_ListItem(heading: "Categories",
+                                                   previewType: .category),
+                                       BT_ListItem(heading: "Terms",
+                                                   previewType: .term)]
+    
+    var body: some View {
+        NavigationView {
+            
+            List {
+                
+                ForEach(BrowseTab.listHeadings) { item in
+                    BT_ListRow(heading: item.heading, previewType: item.previewType)
+                }
+                .listRowInsets(EdgeInsets())        // Sets insets to zero to allow horizontal scrolling to reach display edges.
+                
+            }
+            .navigationTitle("Browse")
+            
+        }
+    }
+    
+}
+
+struct BrowseTab_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseTab()
+    }
+}
