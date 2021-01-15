6a2cc42230f7d8b9e5cb00106f590f582ecbda2d
qitianshi <tianshi.qi@icloud.com>
2021-01-16 00:08:35 +0800

    Add padding to image placeholder text

--- Mimosa/Generic views/ContentPreview.swift
+++ Mimosa/Generic views/ContentPreview.swift
@@ -63,6 +63,7 @@ struct ContentPreview: View {
                     Text("Placeholder image")
                         .applyPlaceholderTextStyle(withBorder: false)
                         .foregroundColor(.white)
+                        .padding()
                 )
             
         }
