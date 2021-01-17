02519bee6fc501a6d58b3bc4d377c11b7a143009
Qi Tianshi <65122416+qitianshi@users.noreply.github.com>
2021-01-17 10:08:01 +0800

    Add search bar and recents to the Search tab (#41)
    * Create SearchBar view
    
    * Add SearchBar and navigation title to SearchTab
    
    * Add Cancel button to SearchBar
    
    * Add Recently Viewed section to SearchTab with placeholder content
    
    * Change ST_Recents to use LazyVStack
    
    * Move ScrollView to outermost layer inside NavigationView

---
+++ Mimosa/Navigation tabs/Search tab/SearchTab.swift
@@ -0,0 +1,45 @@
+// SearchTab.swift
+
+// Created on 31/12/20
+// Contributors: Qi Tianshi
+// Swift 5.0
+
+// Mimosa: Working title; non-working project.
+
+// The Search tab lets users search for formulas by their terms, name, category, and more.
+
+
+import SwiftUI
+
+struct SearchTab: View {
+    
+    @State var searchQuery: String = ""
+    
+    var body: some View {
+        NavigationView {
+            
+            ScrollView {
+                VStack {
+                    
+                    SearchBar(query: $searchQuery, placeholderText: "Formulas, categories, terms, and more")
+                        .padding(.horizontal, -SearchBar.DefaultPadding.horizontal)
+                        .padding(.vertical, -SearchBar.DefaultPadding.vertical)
+                        .padding(.horizontal)
+                    
+                    ST_Recents()
+                        .padding()
+                    
+                }
+            }
+            .navigationTitle("Search")
+            
+        }
+    }
+    
+}
+
+struct SearchTab_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchTab()
+    }
+}
