a8ea8e85752b57fd601a6f47a2729816e5b6f4a5
qitianshi <tianshi.qi@icloud.com>
2021-01-25 10:02:05 +0800

    Add targets for UI tests and unit tests

---
+++ MimosaTests/MimosaTests.swift
@@ -0,0 +1,36 @@
+// MimosaTests.swift
+
+// Created on 25/1/21
+// Contributors: Qi Tianshi
+// Swift 5.0
+
+// Mimosa: Working title; non-working project.
+
+// Unit tests for Mimosa.
+
+
+import XCTest
+
+class MimosaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
