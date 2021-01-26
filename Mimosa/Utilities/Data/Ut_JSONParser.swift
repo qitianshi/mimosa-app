db6462b1e84fb4efd5010d4abb756f0b7d5795a2
qitianshi <tianshi.qi@icloud.com>
2021-01-26 09:37:44 +0800

    Reorganize Utilities directory

---
+++ Mimosa/Utilities/Data/Ut_JSONParser.swift
@@ -0,0 +1,12 @@
+// Ut_JSONParser.swift
+
+// Created on 26/1/21
+// Contributors: Qi Tianshi
+// Swift 5.0
+
+// Mimosa: Working title; non-working project.
+
+// Extracts JSON data.
+
+
+import Foundation
